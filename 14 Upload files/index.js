a8f129ba1e093f963e6ecfad0a64e47ddb0643ab
Marco <126586076+MarcoVerduci@users.noreply.github.com>
2023-07-13 11:10:42 +0200

    Update index.js

--- 14 Upload files/index.js
+++ 14 Upload files/index.js
@@ -1,40 +1,48 @@
 const express = require('express');
 const multer = require('multer');
-const path = require('path');
-const pgp = require('pg-promise')();
-
-const dbConfig = {
-    host: 'localhost',
-    port: 5432,
-    database: 'your_database_name',
-    user: 'your_username',
-    password: 'your_password'
-};
-
-const db = pgp(dbConfig);
-
+const upload = multer({ dest: 'uploads/' });
 const app = express();
+const port = 3000;
+const pgp = require('pg-promise')();
+const connectionString = 'postgres://username:password@localhost:5432/planets_db';
+
+const db = pgp(connectionString);
+
+const createTableQuery = `
+  DROP TABLE IF EXISTS planets;
+  CREATE TABLE planets (
+    id SERIAL NOT NULL PRIMARY KEY,
+    name TEXT NOT NULL,
+    image TEXT
+  );
+`;
+
+async function setupDb() {
+  try {
+    await db.none(createTableQuery);
+    console.log('Database setup complete.');
+  } catch (error) {
+    console.error('Error setting up database:', error);
+  }
+}
+
 app.use(express.json());
 
-const storage = multer.diskStorage({
-    destination: 'uploads',
-    filename: function (req, file, cb) {
-        const ext = path.extname(file.originalname);
-        cb(null, Date.now() + ext);
-    }
+app.post('/planets/:id/image', upload.single('image'), async (req, res) => {
+  const { id } = req.params;
+  const { path } = req.file;
+
+  try {
+    await db.none('UPDATE planets SET image=$2 WHERE id=$1', [id, path]);
+    res.sendStatus(200);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Internal server error' });
+  }
 });
 
-const upload = multer({ storage });
-
-app.post('/api/planets/:id/image', upload.single('image'), async (req, res) => {
-    const planetId = parseInt(req.params.id);
-    const filePath = req.file.path;
-
-    try {
-        await db.none('UPDATE planets SET image = $1 WHERE id = $2', [filePath, planetId]);
-        res.json({ msg: 'Image uploaded successfully' });
-    } catch (error) {
-        console.error('Error uploading image:', error);
-        res.status(500).json({ error: 'Failed to upload image' });
-    }
+setupDb().then(() => {
+  app.listen(port, () => {
+    console.log(`App listening at http://localhost:${port}`);
+  });
 });
