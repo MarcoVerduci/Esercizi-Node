e0f6c0af6654500aebd06e86d4c19d8b675211fa
Marco <126586076+MarcoVerduci@users.noreply.github.com>
2023-07-13 11:18:52 +0200

    Update index.js

--- 16 JWT Auth part 2/index.js
+++ 16 JWT Auth part 2/index.js
@@ -1,65 +1,101 @@
 const express = require('express');
 const passport = require('passport');
-const { Strategy: JwtStrategy, ExtractJwt } = require('passport-jwt');
-const jwt = require('jsonwebtoken');
+const passportJWT = require('passport-jwt');
+const { Strategy, ExtractJwt } = passportJWT;
 const dotenv = require('dotenv');
-const pgp = require('pg-promise')();
+const pg = require('pg');
+const jwt = require('jsonwebtoken');
 
 dotenv.config();
 
-const dbConfig = {
-    host: 'localhost',
-    port: 5432,
-    database: 'your_database_name',
-    user: 'your_username',
-    password: 'your_password'
+const pool = new pg.Pool();
+
+const jwtOptions = {
+  secretOrKey: process.env.SECRET,
+  jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken()
 };
 
-const db = pgp(dbConfig);
-
-app.post('/api/users/signup', async (req, res) => {
-    const { username, password } = req.body;
-
-    try {
-        const existingUser = await db.oneOrNone('SELECT * FROM users WHERE username = $1', username);
-        if (existingUser) {
-            return res.status(400).json({ error: 'Username is already taken' });
-        }
-
-        const newUser = await db.one(
-            'INSERT INTO users (username, password) VALUES ($1, $2) RETURNING id, username',
-            [username, password]
-        );
-
-        res.json({ msg: 'Signup successful. Now you can log in.' });
-    } catch (error) {
-        console.error('Error signing up:', error);
-        res.status(500).json({ error: 'Failed to sign up' });
+const strategy = new Strategy(jwtOptions, (payload, done) => {
+  const { id } = payload;
+  pool.query('SELECT * FROM users WHERE id = $1', [id], (err, result) => {
+    if (err) {
+      return done(err, false);
     }
+
+    const user = result.rows[0];
+
+    if (user) {
+      return done(null, user);
+    } else {
+      return done(null, false);
+    }
+  });
 });
 
-app.post('/api/users/login', async (req, res) => {
-    const { username, password } = req.body;
-
-    try {
-        const user = await db.oneOrNone('SELECT * FROM users WHERE username = $1', username);
-        if (!user) {
-            return res.status(404).json({ error: 'User not found' });
-        }
-
-        if (password !== user.password) {
-            return res.status(401).json({ error: 'Incorrect password' });
-        }
-
-        const token = jwt.sign({ id: user.id, username: user.username }, process.env.SECRET);
-
-        res.json({ token, id: user.id, username: user.username });
-    } catch (error) {
-        console.error('Error logging in:', error);
-        res.status(500).json({ error: 'Failed to log in' });
-    }
-});
+passport.use(strategy);
 
 const app = express();
+
 app.use(express.json());
+
 app.use(passport.initialize());
+
+app.post('/users/signup', (req, res) => {
+  const { username, password } = req.body;
+    
+  if (!username || !password) {
+    return res.status(400).json({ error: 'Username and password are required.' });
+  }
+
+  pool.query('INSERT INTO users (username, password) VALUES ($1, $2) RETURNING id', [username, password], (err, result) => {
+    if (err) {
+      console.error(err);
+      return res.status(500).json({ error: 'An error occurred while creating the user.' });
+    }
+
+    const userId = result.rows[0].id;
+    return res.json({ msg: 'Signup successful. Now you can log in.' });
+  });
+});
+
+app.post('/users/login', (req, res) => {
+  const { username, password } = req.body;
+
+  if (!username || !password) {
+    return res.status(400).json({ error: 'Username and password are required.' });
+  }
+
+  pool.query('SELECT * FROM users WHERE username = $1', [username], (err, result) => {
+    if (err) {
+      console.error(err);
+      return res.status(500).json({ error: 'An error occurred while logging in.' });
+    }
+
+    const user = result.rows[0];
+
+    if (!user) {
+      return res.status(401).json({ error: 'Invalid username or password.' });
+    }
+
+    if (password !== user.password) {
+      return res.status(401).json({ error: 'Invalid username or password.' });
+    }
+
+    const payload = { id: user.id, username: user.username };
+    const token = jwt.sign(payload, process.env.SECRET);
+
+    pool.query('UPDATE users SET token = $1 WHERE id = $2', [token, user.id], (err) => {
+      if (err) {
+        console.error(err);
+        return res.status(500).json({ error: 'An error occurred while logging in.' });
+      }
+
+      return res.json({ token, id: user.id, username: user.username });
+    });
+  });
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
